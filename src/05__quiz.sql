a1170bf4304de400ef562553bdf3aafeeb478b66
Rushm <gcalku@gmail.com>
2025-06-01 21:38:50 +0700

    update

--- src/05__quiz.sql
+++ src/05__quiz.sql
@@ -1,2 +1,18 @@
 USE MCFGA_BIZ_DEMO09102024
 GO
+
+-- ============================================
+-- ใช้ T_SALESORDERD อยากทราบราคาสุทธิรวมทั ้งใบ SALESORDER ของแต่ละใบ
+-- ============================================
+
+SELECT
+    VOUCHERNO,
+    INPDATE,
+    SUM(UNITPRICE * QTY) AS TOTAL_SALES,
+    SUM(INPAMOUNT_SC) AS TOTAL_PRICE
+FROM
+    T_SALESORDERD
+GROUP BY
+    VOUCHERNO, INPDATE
+
+
