13a68e97d6af15b6d80456ac0af92c8a8dc20ad9
Rushm <gcalku@gmail.com>
2025-06-01 20:30:45 +0700

    update

--- src/03__quiz.sql
+++ src/03__quiz.sql
@@ -5,13 +5,12 @@ GO
 -- ใช้ T_SALESORDERH อยากทราบราคาหลังจากคำนวณอัตราแลกเปลี่ยนค่าเงินในกรณีที่ CURRENCY ไม่ใช่ “THB”
 -- ============================================
 
-SELECT
-    H.CURRENCYCODE,
-    H.TOTALAMOUNT_SC,
-    CASE
-        WHEN H.CURRENCYCODE = 'USD' THEN H.TOTALAMOUNT_SC * 32.6
-        WHEN H.CURRENCYCODE = 'JPY' THEN H.TOTALAMOUNT_SC * 0.23
-        ELSE H.TOTALAMOUNT_SC
-        END AS IN_THB
+SELECT CURRENCYCODE,
+       TOTALAMOUNT_SC,
+       CASE
+           WHEN CURRENCYCODE = 'USD' THEN TOTALAMOUNT_SC * 32.6
+           WHEN CURRENCYCODE = 'JPY' THEN TOTALAMOUNT_SC * 0.23
+           ELSE TOTALAMOUNT_SC
+           END AS IN_THB
 FROM
-    T_SALESORDERH H
+    T_SALESORDERH
--- src/04__quiz.sql
+++ src/04__quiz.sql
@@ -1,2 +1,36 @@
 USE MCFGA_BIZ_DEMO09102024
 GO
+
+-- ============================================
+-- ใช้ T_SALESORDERH อยากทราบว่าใบ SALESORDER ไหนที่ถูกคัดลอกมา
+-- และเปรียบเทียบระหว่างใบต้นฉบับและใบคัดลอก
+-- ============================================
+
+
+SELECT
+    C.VOUCHERNO,
+    C.INPDATE,
+    C.CUSTOMERCODE,
+    C.CURRENCYCODE,
+    C.RATE,
+    C.TOTALAMOUNT_SC,
+    C.DEPTCODE,
+    C.INCHARGECODE,
+    C.COMPANY,
+
+    O.VOUCHERNO,
+    O.INPDATE,
+    O.DEPTCODE,
+    O.WAREHOUSE,
+    O.GOODSCODE,
+    O.UNITPRICE,
+    O.QTY,
+    O.INPAMOUNT_SC
+FROM
+    T_SALESORDERH AS C
+    INNER JOIN T_SALESORDERD AS O
+        ON O.VOUCHERNO = C.COPYVOUCHERNO
+WHERE
+    C.COPYVOUCHERNO IS NOT NULL
+        AND LEN(C.COPYVOUCHERNO) > 0
+
