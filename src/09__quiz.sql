6c11ce65539ad35bfde04dae65f63258bc3a6332
rushmi0 <gcalku@gmail.com>
2025-06-02 10:56:05 +0700

    update quiz

--- src/09__quiz.sql
+++ src/09__quiz.sql
@@ -1,2 +1,45 @@
 USE MCFGA_BIZ_DEMO09102024
 GO
+
+
+-- ============================================
+-- ใช้ T_SALESORDERD อยากทราบยอดขายเฉลี่ยของ SALESORDER ในแต่ละเดือน
+-- ============================================
+
+
+SELECT
+    MONTH(INPDATE) AS [MONTH],
+    CAST(AVG(INPAMOUNT_SC) AS DECIMAL(10, 2)) AS [AVG_MOUNT]
+FROM
+    T_SALESORDERD
+GROUP BY
+    MONTH(INPDATE);
+
+
+
+
+/*
+SELECT DISTINCT
+    YEAR(INPDATE) AS [YEAR]
+FROM
+    T_SALESORDERD
+
+SELECT DISTINCT
+    MONTH(INPDATE) AS [MONTH]
+FROM
+    T_SALESORDERD
+
+
+DECLARE @AVG_SC INT = (
+    SELECT
+        MONTH(INPDATE),
+        AVG(INPAMOUNT_SC)
+    FROM
+        T_SALESORDERD
+    WHERE
+        MONTH(INPDATE) = 9
+    group by INPDATE)
+
+SELECT @AVG_SC;
+
+*/
